74b532a60f6f6f41d46d44bf85f029b75df8586e
Isaias Batista <isaiasbatistapy@gmail.com>
2024-11-13 11:34:08 -0300

    condicional When

---
+++ src/condicionalWhen.kt
@@ -0,0 +1,21 @@
+fun main(args: Array<String>) {
+    val idade = 150
+
+    when(idade){
+      in 1..17 -> println("você é menor")
+      in 18 .. 29 ->  println("você é Jovem")
+        in 30..60 -> println("você é um Senhor")
+        else -> println("Melhor idade")
+    }
+}
+
+   /* if (idade in 1..17){
+        println("você é menor")
+    }else if (idade in 18..28){
+        println("você é Jovem")
+    }else if (idade in 30..60){
+        println("você é um Senhor")
+    }
+}
+*/
+

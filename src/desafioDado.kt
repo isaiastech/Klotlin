6a43d68714d78e2e2205cbecec99a90736f125a4
Isaias Batista <isaiasbatistapy@gmail.com>
2024-11-14 11:46:41 -0300

    estrutura condicional

---
+++ src/desafioDado.kt
@@ -0,0 +1,15 @@
+
+
+fun main() {
+    print("Escolha um número entre 0 e 6? ")
+    val palpite = readLine()?.toInt()!!
+    val dado = (1..6).random()
+    println("Palpite: $palpite")
+    println("O número soreteado foi: $dado")
+    when(palpite == dado){
+        true -> println("Você acertou o número")
+        false -> println("Você errou !!")
+    }
+
+}
+
---
+++ src/estruraWhile.kt
@@ -0,0 +1,8 @@
+fun main() {
+    val number = arrayListOf<Int>()
+    var tamanho = 0
+    while ( number.size != 10){
+        number.add(tamanho++)
+        println("Valor: ${number.size}")
+    }
+}
